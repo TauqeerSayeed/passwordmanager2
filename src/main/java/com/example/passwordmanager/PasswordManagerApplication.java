d310f538985efce90b962bd386dd361340959e3f
Tauqeer Sayeed <20btcs042hy@manuu.edu.in>
2025-10-20 11:37:55 +0530

    commit time zone

--- src/main/java/com/example/passwordmanager/PasswordManagerApplication.java
+++ src/main/java/com/example/passwordmanager/PasswordManagerApplication.java
@@ -1,7 +1,10 @@
 package com.example.passwordmanager;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class PasswordManagerApplication {
@@ -9,6 +12,13 @@ public class PasswordManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PasswordManagerApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		// Set default timezone
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+		System.out.println("Application running in timezone: " + TimeZone.getDefault().getID());
 	}
 
 }
